eac80c773d3dcf4cc4570d46ea9ee2f9f7a389de
HR-tech Lab <59562246+HRtech-lab@users.noreply.github.com>
2022-05-03 12:41:38 +0500

    Create vcf_to_dataFrame.py

---
+++ VCF_to_dataframe/vcf_to_dataFrame.py
@@ -0,0 +1,16 @@
+#Function to convert vcf entries into dataframe
+import io
+import os
+import pandas as pd
+
+
+def read_vcf(path):
+    with open(path, 'r') as f:
+        lines = [l for l in f if not l.startswith('##')]
+    return pd.read_csv(
+        io.StringIO(''.join(lines)),
+        dtype={'#CHROM': str, 'POS': int, 'ID': str, 'REF': str, 'ALT': str,
+               'QUAL': str, 'FILTER': str, 'INFO': str},
+        sep='\t'
+    ).rename(columns={'#CHROM': 'CHROM'})
+  #note: no other online available tool directly converts vcf to dataframe entries with all features accessible.
